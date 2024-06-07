9f6b37dcc6e559034f0d019310cfdb73b8b81850
Asgeir Albretsen <bjellanda@gmail.com>
2024-06-07 12:48:51 +0200

    Changed container styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import OfficialTab from '../play/official';
 import CommunityTab from '../play/community';
-import { useTheme } from 'tamagui';
+import { View, useTheme } from 'tamagui';
 import { Dimensions } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
@@ -10,31 +10,33 @@ export default function TabLayout() {
     const theme = useTheme();
 
     return (
-        <Tab.Navigator screenOptions={{
-            swipeEnabled: false,
-            tabBarStyle: {
-                backgroundColor: 'transparent',
-                shadowColor: 'transparent',
-                width: '70%',
-                marginHorizontal: 16
-            },
-            tabBarLabelStyle: {
-                backgroundColor: 'transparent',
-                fontFamily: 'InterBold',
-                fontSize: 16,
-                textTransform: 'none'
-            },
-            tabBarIndicatorStyle: {
-                backgroundColor: theme.color.val,
-                height: 2
-            },
-            tabBarActiveTintColor: theme.color.val,
-            tabBarPressColor: theme.color.val,
-        }} initialLayout={{
-            width: Dimensions.get('window').width
-        }}>
-            <Tab.Screen name="Official" component={OfficialTab} />
-            <Tab.Screen name="Community" component={CommunityTab} />
-        </Tab.Navigator>
+        <View flex={1} backgroundColor={'$background'}>
+            <Tab.Navigator screenOptions={{
+                swipeEnabled: false,
+                tabBarStyle: {
+                    backgroundColor: 'transparent',
+                    shadowColor: 'transparent',
+                    width: '70%',
+                    marginHorizontal: 16
+                },
+                tabBarLabelStyle: {
+                    backgroundColor: 'transparent',
+                    fontFamily: 'InterBold',
+                    fontSize: 16,
+                    textTransform: 'none'
+                },
+                tabBarIndicatorStyle: {
+                    backgroundColor: theme.color.val,
+                    height: 2
+                },
+                tabBarActiveTintColor: theme.color.val,
+                tabBarPressColor: theme.color.val,
+            }} initialLayout={{
+                width: Dimensions.get('window').width
+            }}>
+                <Tab.Screen name="Official" component={OfficialTab} />
+                <Tab.Screen name="Community" component={CommunityTab} />
+            </Tab.Navigator>
+        </View>
     );
 }
