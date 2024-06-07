51935c9f3290183afa017b317c1ac6abd180d78a
Asgeir Albretsen <bjellanda@gmail.com>
2024-06-07 13:04:01 +0200

    Moved List components to /components

--- app/play/official.tsx
+++ app/play/official.tsx
@@ -1,8 +1,8 @@
 import { View, Text } from 'react-native';
 import { StyledContainer } from '../../src/styles/styles';
 import { Link } from 'expo-router';
-import List from '../../src/UI/list/List';
-import ListItem from '../../src/UI/list/ListItem';
+import List from '../../src/components/list/List';
+import ListItem from '../../src/components/list/ListItem';
 import { supabase } from '../../supabase';
 
 export default function Tab() {
