64df813060c9f8cf8b048c4d4e06b76ff9e6eedd
Asgeir Albretsen <bjellanda@gmail.com>
2024-06-09 20:29:58 +0200

    Added cover images

--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -1,5 +1,5 @@
-import { View, Text, SizableText, XStack, Button } from "tamagui";
-import { Heart, Eye } from '@tamagui/lucide-icons'
+import { View, Text, SizableText, XStack, Button, Image } from "tamagui";
+import { Heart, Eye } from '@tamagui/lucide-icons';
 
 export default function ListItem(props: any) {
     const { item } = props;
@@ -7,16 +7,23 @@ export default function ListItem(props: any) {
     return (
         <View backgroundColor={'$main2'} borderWidth={1} borderRadius={10} borderColor={'$main6'}>
             <View margin={16} gap={16}>
-                <View height={100} backgroundColor={'$main4'} borderRadius={10}>
+                {item.cover ?
+                    <Image height={100} source={{
+                        uri: `https://eslrohuhvzvuxvueuziv.supabase.co/storage/v1/object/public/covers/${item.id}.png`,
+                    }} borderRadius={10}>
 
-                </View>
+                    </Image>
+                    :
+                    <View height={100} backgroundColor={'$main4'} borderRadius={10}>
+
+                    </View>}
                 <View>
                     <SizableText size={'$8'} fontWeight={900}>{item.title}</SizableText>
                     <SizableText size={'$4'} fontWeight={400}>by {item.profiles.username}</SizableText>
                 </View>
                 <View>
-                    <XStack flex={1} justifyContent="space-between">
-                        <XStack flex={1} gap={16}>
+                    <XStack justifyContent="space-between">
+                        <XStack gap={16}>
                             <XStack gap={4} alignItems={"center"}>
                                 <Heart />
                                 <Text>1k likes</Text>
