eb35f1010ae8c924fbe3c32eb96b93594ae09e81
Asgeir Albretsen <bjellanda@gmail.com>
2024-06-09 19:34:01 +0200

    Added likes, plays and Play to ListItem

--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -1,4 +1,5 @@
-import { View, Text, SizableText } from "tamagui";
+import { View, Text, SizableText, XStack, Button } from "tamagui";
+import { Heart, Eye } from '@tamagui/lucide-icons'
 
 export default function ListItem(props: any) {
     const { item } = props;
@@ -12,6 +13,23 @@ export default function ListItem(props: any) {
                 <View>
                     <SizableText size={'$8'} fontWeight={900}>{item.title}</SizableText>
                     <SizableText size={'$4'} fontWeight={400}>by {item.profiles.username}</SizableText>
+                </View>
+                <View>
+                    <XStack flex={1} justifyContent="space-between">
+                        <XStack flex={1} gap={16}>
+                            <XStack gap={4} alignItems={"center"}>
+                                <Heart />
+                                <Text>1k likes</Text>
+                            </XStack>
+                            <XStack gap={4} alignItems={"center"}>
+                                <Eye />
+                                <Text>14k plays</Text>
+                            </XStack>
+                        </XStack>
+                        <View>
+                            <Button borderRadius={100} backgroundColor={'$main4'}> Play </Button>
+                        </View>
+                    </XStack>
                 </View>
             </View>
         </View>
