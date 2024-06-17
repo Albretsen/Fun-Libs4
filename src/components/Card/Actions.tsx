3742112fd1c673b6664eacd808d532c6db1363d8
Asgeir Albretsen <bjellanda@gmail.com>
2024-06-17 19:57:11 +0200

    Fixed font loading issue (upgraded all packages) Fix for key (...) spread error: downgrade React from 18.3.x to 18.2.x Added unqiue key to List

--- src/components/Card/Actions.tsx
+++ src/components/Card/Actions.tsx
@@ -6,13 +6,13 @@ export default function Actions() {
     return (
         <View borderWidth={1} borderColor={'$main6'} borderRadius={8} height={66} backgroundColor={'$background'}>
             <XStack justifyContent="space-evenly" alignItems="center" flex={1}>
-                <Button icon={Heart} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={0}>
+                <Button icon={Heart} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={'unset'} >
                     28 likes
                 </Button>
-                <Button icon={Share} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={0}>
+                <Button icon={Share} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={'unset'} >
                     Share
                 </Button>
-                <Button icon={User} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={0}>
+                <Button icon={User} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={'unset'} >
                     Profile
                 </Button>
             </XStack>
