f64eb21b76d96bc8c6f0fa26a0c0c89825d598c9
Asgeir Albretsen <bjellanda@gmail.com>
2024-07-10 20:51:00 +0200

    Animated SkeletonCard

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,5 +1,5 @@
 import { FlashList } from "@shopify/flash-list";
-import { Spinner, SizableText, View } from "tamagui";
+import { SizableText, View } from "tamagui";
 import {
     useInfiniteQuery,
     useQueryClient,
@@ -38,7 +38,7 @@ export default function List(props: any) {
     });
 
     useEffect(() => {
-        let temp_items: any = [];
+        const temp_items: any = [];
         if (!data) return;
         for (let i = 0; i < data.pages.length; i++) {
             for (let j = 0; j < data.pages[i].data.length; j++) {
@@ -58,15 +58,15 @@ export default function List(props: any) {
         }
     };
 
-    if (isFetching && !isFetchingNextPage) {
-        return (
-            <View flex={1}>
-                {[...Array(5)].map((_, index) => (
-                    <SkeletonCard key={index} />
-                ))}
-            </View>
-        );
-    }
+    // if (isFetching && !isFetchingNextPage) {
+    //     return (
+    //         <View flex={1}>
+    //             {[...Array(5)].map((_, index) => (
+    //                 <SkeletonCard key={index} />
+    //             ))}
+    //         </View>
+    //     );
+    // }
 
     if (isError) {
         return (
@@ -77,11 +77,17 @@ export default function List(props: any) {
     return (
         <View flex={1}>
             <FlashList
-                data={items}
+                data={isFetching && !isFetchingNextPage ? [] : items}
                 renderItem={({ item }: any) => <ListItem item={item} variant={'listItem'} />}
                 keyExtractor={(item: any) => item.id}
                 estimatedItemSize={80}
-                ListEmptyComponent={<SizableText size={'$5'}>No results</SizableText>}
+                ListEmptyComponent={
+                    <View flex={1}>
+                        {[...Array(5)].map((_, index) => (
+                            <SkeletonCard key={index} />
+                        ))}
+                    </View>
+                }
                 onRefresh={refresh}
                 onEndReached={onEndReached}
                 onEndReachedThreshold={0.5}
