9199d98293f15acae22ecb831969e53cbd15d1fc
Asgeir Albretsen <bjellanda@gmail.com>
2024-07-23 18:49:36 +0200

    Added loading indicator to save button

--- src/components/Card/Actions/ActionButton.tsx
+++ src/components/Card/Actions/ActionButton.tsx
@@ -1,16 +1,18 @@
 import { Button } from "tamagui";
+import { Spinner } from "tamagui";
 
 interface ActionButtonProps {
     icon: any;
     label: string;
     onPress?: () => void;
+    loading?: boolean;
 }
 
 export default function ActionButton(props: ActionButtonProps) {
-    const { icon, label, onPress } = props;
+    const { icon, label, onPress, loading } = props;
 
     return (
-        <Button onPress={onPress} icon={icon} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={'unset'} >
+        <Button onPress={onPress} icon={loading ? <Spinner color={'$main12'} /> : icon} scaleIcon={1.4} flexDirection="column" backgroundColor="$colorTransparent" space={'unset'} >
             {label}
         </Button>
     )
