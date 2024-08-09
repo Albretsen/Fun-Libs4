42afd13f70229b9676ca05d6ba453c4e2085b6fd
Asgeir Albretsen <bjellanda@gmail.com>
2024-08-09 17:33:41 +0200

    Fixed header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ export default function Header() {
                                 </XStack>
                             </View>
                         }
-                    </SafeAreaView >
+                    </View >
             }}
         />
     )
