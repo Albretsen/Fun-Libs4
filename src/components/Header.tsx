ddb6d6b6a90dd7f0e010dd8a433e42ff99de59f7
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-08-11 21:45:44 +0200

    Added proper header for profile screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { Image, Input, View, XStack, Text, SizableText } from "tamagui";
+import { Image, Input, View, XStack, Text, SizableText, Button } from "tamagui";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useAuth from "../hooks/useAuth";
-import { Bell, ArrowLeft, Menu } from "@tamagui/lucide-icons";
+import { Bell, ArrowLeft, Menu, X, Pen } from "@tamagui/lucide-icons";
 import { TouchableOpacity } from "react-native";
 import { router } from "expo-router";
 
@@ -16,28 +16,57 @@ export default function Header() {
                 headerShown: true,
                 header: (props) =>
                     <SafeAreaView>
-                        {!props.back ?
-                            <View backgroundColor={'$background'}>
-                                <XStack margin={16} height={32} alignItems="center" >
-                                    <Menu />
-                                    {/* <Input placeholder="Search" height={'100%'} flex={1} marginHorizontal={16} borderRadius={999} /> */}
-                                    <SizableText size={'$6'} flex={1} marginHorizontal={16} textAlign="center" fontWeight={'500'}>{props.options.title}</SizableText>
-                                    <TouchableOpacity onPress={() => router.navigate("/profile")} hitSlop={8}>
-                                        <Image height={'100%'} width={32} backgroundColor={'$main6'} objectFit="contain" source={{
-                                            uri: session?.user?.avatar_url ? session.user.avatar_url : 'https://eslrohuhvzvuxvueuziv.supabase.co/storage/v1/object/public/avatars/no-avatar.png',
-                                        }} borderRadius={1000} />
-                                    </TouchableOpacity>
+                        {props.route.name === "profile" ?
+                            <View style={{
+                                height: 0,
+                            }}>
+                                <XStack margin={16} height={32} justifyContent="space-between" alignItems="center">
+                                    <Button
+                                        borderWidth={1}
+                                        borderColor={'$main6'}
+                                        aspectRatio={1}
+                                        backgroundColor={'$main4'}
+                                        borderRadius={50}
+                                    >
+                                        <X />
+                                    </Button>
+                                    <Button
+                                        borderWidth={1}
+                                        borderColor={'$main6'}
+                                        backgroundColor={'$main4'}
+                                        height={40}
+                                        borderRadius={50}
+                                        gap={0}
+                                    >
+                                        <Pen />
+                                        <SizableText>Edit</SizableText>
+                                    </Button>
                                 </XStack>
+                                {/* <SizableText>{JSON.stringify(props)}</SizableText> */}
                             </View>
                             :
-                            < View backgroundColor={'$background'}>
-                                <XStack margin={16} height={16} alignItems="center">
-                                    <TouchableOpacity onPress={() => router.back()} hitSlop={16} style={{ height: '100%' }}>
-                                        <ArrowLeft />
-                                    </TouchableOpacity>
-                                </XStack>
-                            </View>
-                        }
+                            <>{!props.back ?
+                                <View backgroundColor={'$background'}>
+                                    <XStack margin={16} height={32} alignItems="center" >
+                                        <Menu />
+                                        {/* <Input placeholder="Search" height={'100%'} flex={1} marginHorizontal={16} borderRadius={999} /> */}
+                                        <SizableText size={'$6'} flex={1} marginHorizontal={16} textAlign="center" fontWeight={'500'}>{props.options.title}</SizableText>
+                                        <TouchableOpacity onPress={() => router.navigate("/profile")} hitSlop={8}>
+                                            <Image height={'100%'} width={32} backgroundColor={'$main6'} objectFit="contain" source={{
+                                                uri: session?.user?.avatar_url ? session.user.avatar_url : 'https://eslrohuhvzvuxvueuziv.supabase.co/storage/v1/object/public/avatars/no-avatar.png',
+                                            }} borderRadius={1000} />
+                                        </TouchableOpacity>
+                                    </XStack>
+                                </View>
+                                :
+                                < View backgroundColor={'$background'}>
+                                    <XStack margin={16} height={16} alignItems="center">
+                                        <TouchableOpacity onPress={() => router.back()} hitSlop={16} style={{ height: '100%' }}>
+                                            <ArrowLeft />
+                                        </TouchableOpacity>
+                                    </XStack>
+                                </View>
+                            }</>}
                     </SafeAreaView >
             }}
         />
