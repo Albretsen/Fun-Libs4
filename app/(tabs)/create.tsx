50b7e33cf2fa91df378fc22b46bce10672a56974
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-10-03 17:40:08 +0200

    Fixed create menu keyboard on iOS not being dismissable

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,33 +1,66 @@
-import { StyledContainer } from '../../src/styles/styles';
-import CreateCard from '../../src/components/CreateCard/CreateCard';
-import KeyboardExtender from '../../src/components/Misc/KeyboardExtender';
-import { SizableText, View } from 'tamagui';
-import SignUp from '../../src/components/Auth/SignUp';
-import useAuth from '../../src/hooks/useAuth';
 import { Stack } from 'expo-router';
 import Header from '../../src/components/Header';
+import { View, SizableText } from "tamagui";
+import CreateCard from '../../src/components/CreateCard/CreateCard';
+import SignUp from '../../src/components/Auth/SignUp';
+import { StyledContainer } from '../../src/styles/styles';
+import useAuth from '../../src/hooks/useAuth';
+import KeyboardExtender from '../../src/components/Misc/KeyboardExtender';
+import { KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native';
 
 export default function Tab() {
-
     const { session } = useAuth();
 
-    return (
-        <>
-            {!session?.user.is_anonymous ?
-                <View flex={1}>
-                    <Header />
-                    <StyledContainer>
-                        <CreateCard />
-                    </StyledContainer>
-                    <KeyboardExtender />
-                </View>
-                :
-                <StyledContainer >
-                    <View gap={8}>
-                        <SizableText size={'$5'}>⚠️ Create an account to publish stories!</SizableText>
-                        <SignUp />
+    if (Platform.OS === 'ios') {
+        return (
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={'padding'}
+                keyboardVerticalOffset={110}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                        <View style={{ flex: 1 }}>
+                            {!session?.user.is_anonymous ? (
+                                <View flex={1}>
+                                    <Header />
+                                    <StyledContainer>
+                                        <CreateCard />
+                                    </StyledContainer>
+                                    <KeyboardExtender />
+                                </View>
+                            ) : (
+                                <StyledContainer>
+                                    <View gap={8}>
+                                        <SizableText size={'$5'}>⚠️ Create an account to publish stories!</SizableText>
+                                        <SignUp />
+                                    </View>
+                                </StyledContainer>
+                            )}
+                        </View>
+                    </ScrollView>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        );
+    } else {
+        return (
+            <>
+                {!session?.user.is_anonymous ?
+                    <View flex={1}>
+                        <Header />
+                        <StyledContainer>
+                            <CreateCard />
+                        </StyledContainer>
+                        <KeyboardExtender />
                     </View>
-                </StyledContainer>}
-        </>
-    );
+                    :
+                    <StyledContainer >
+                        <View gap={8}>
+                            <SizableText size={'$5'}>⚠️ Create an account to publish stories!</SizableText>
+                            <SignUp />
+                        </View>
+                    </StyledContainer>}
+            </>
+        )
+    }
 }
