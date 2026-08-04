aa089b12ea8bda395a39948a016afac06be61832
hallvard <hallvard@unimicro.no>
2026-08-04 23:25:16 +0200

    chore :: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,8 @@ export default [
 		},
 	},
 	...tseslint.configs.recommended,
-	eslintConfigPrettier,
+  eslintConfigPrettier,
+	prettierConfig,
 	{
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
